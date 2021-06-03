0b22999bc0458d0c506a44a58c00db1670d248c7
punsoca <punsoca@gmail.com>
2021-06-03 14:10:12 -0700

    Flask flashing message on successful add/edit/delete

---
+++ Lesson3/16-Messaing-Flashing-Quiz/project.py
@@ -0,0 +1,91 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+app = Flask(__name__)
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+# List Restaurant Menus
+@app.route('/')
+@app.route('/restaurants/<int:restaurant_id>/menu')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id)
+    return render_template(
+        'menu.html', restaurant=restaurant, items=items, restaurant_id=restaurant_id)
+
+
+# Add New item (including price and description of new item)
+@app.route('/restaurants/<int:restaurant_id>/new', methods=['GET', 'POST'])
+def newMenuItem(restaurant_id):
+
+    if request.method == 'POST':
+        #  the POST method will process data received from input form from the newmenuitem.html
+        newItem = MenuItem(name=request.form['menuName'], description=request.form[
+                           'description'], price=request.form['price'], restaurant_id=restaurant_id)
+        session.add(newItem)
+        session.commit()
+
+        # add a flash "prompt" message when new item created successfully
+        flash("new menu item created!")
+
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        # the GET method will just render the newmenuitem template
+        return render_template('newmenuitem.html', restaurant_id=restaurant_id)
+
+# Edit New item (may change the menu name, price and/or description of selected menu)
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit',
+           methods=['GET', 'POST'])
+def editMenuItem(restaurant_id, menu_id):
+    editedItem = session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method == 'POST':
+        #  the POST method will process data received from input form from the editmenuitem.html
+        if request.form['name']:
+            editedItem.name = request.form['name']
+            editedItem.price = request.form['price']
+            editedItem.description = request.form['description']
+        session.add(editedItem)
+        session.commit()
+
+        # add a flash "prompt" message when edit operation is successful
+        flash("Edit menu item success!")
+
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        # the GET method will just render the editmenuitem template
+        return render_template(
+            'editmenuitem.html', restaurant_id=restaurant_id, item=editedItem)
+
+
+# Delete selected menu item
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete',
+           methods=['GET', 'POST'])
+def deleteMenuItem(restaurant_id, menu_id):
+    itemToDelete = session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method == 'POST':
+        session.delete(itemToDelete)
+        session.commit()
+
+        # add a flash "prompt" message when delete operation is successful
+        flash("Delete menu item success!")
+
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        # the GET method will just render the deletemenuitem template
+        return render_template('deletemenuitem.html', restaurant_id=restaurant_id, item=itemToDelete)
+
+
+if __name__ == '__main__':
+
+    # to allow for flash messaging, an app.secret_key needs to be set up
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
